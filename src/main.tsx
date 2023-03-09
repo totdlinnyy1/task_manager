e09674dc4771039460a9f6122355aac70e1e2fab
tot_dlinnyy <gnngr333@gmail.com>
2023-03-09 21:14:19 +0700

    feat: add ui libriry
    Add mantine ui libriry https://mantine.dev/

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,11 @@
+import {MantineProvider} from '@mantine/core'
 import {createRoot} from 'react-dom/client'
 import {RouterProvider} from 'react-router-dom'
 
 import router from './router/router'
 
 createRoot(document.getElementById('root') as HTMLElement).render(
-  <RouterProvider router={router} />
+  <MantineProvider withNormalizeCSS withGlobalStyles>
+    <RouterProvider router={router} />
+  </MantineProvider>
 )
