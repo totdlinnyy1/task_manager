87685a8c797cde928988c552ba8857d8b8612e3a
tot_dlinnyy <gnngr333@gmail.com>
2023-03-13 14:22:42 +0700

    feat: add info to main page

--- src/pages/main/Main.tsx
+++ src/pages/main/Main.tsx
@@ -1,4 +1,4 @@
-import {Box} from '@mantine/core'
+import {Box, Container, Flex, Image, Text, Button} from '@mantine/core'
 import {FC} from 'react'
 
 import Header from '../../components/header/Header'
@@ -7,6 +7,25 @@ const Main: FC = () => {
   return (
     <Box>
       <Header />
+      <Container>
+        <Flex justify='space-between'>
+          <Box>
+            <Text mb={10}>Какая-то информация</Text>
+            <Button.Group orientation='vertical'>
+              <Button mb={10}>Регистрация</Button>
+              <Button variant='subtle' compact uppercase>
+                О приложении
+              </Button>
+            </Button.Group>
+          </Box>
+          <Box>
+            <Image
+              height='500px'
+              src='https://krot.info/uploads/posts/2022-03/1646169088_48-krot-info-p-kot-prikol-smeshnie-foto-53.jpg'
+            />
+          </Box>
+        </Flex>
+      </Container>
     </Box>
   )
 }
